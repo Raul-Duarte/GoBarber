c6154d04ab30132202c05f12e6f13668f7abce33
Raul-Duarte <raulduarte.info@gmail.com>
2021-01-26 15:35:06 -0300

    ValidandoAgendamentos

--- goBarberBack/src/database/index.js
+++ goBarberBack/src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize, { Model } from 'sequelize'
 
 import User from '../app/models/User'
 import File from '../app/models/File'
+import Appontiment from '../app/models/Appointment'
 
 import databaseConfig from '../config/database'
 
-const models = [User, File]
+const models = [User, File, Appontiment]
 
 class Database {
     constructor() {
---
+++ goBarberBack/src/database/migrations/20210125161911-create-appointments.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    return await queryInterface.createTable('appointments', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      date: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNUl: true,
+      },
+      provider_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNUl: true,
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
+
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+
+  },
+
+  down: async (queryInterface) => {
+
+    return await queryInterface.dropTable('users');
+
+  }
+};
